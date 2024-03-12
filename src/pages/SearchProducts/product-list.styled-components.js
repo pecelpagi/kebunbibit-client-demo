be41e3e3c389783d02904d4d58da60dd9300b4ff
Galuh Ramadhan <galuh.html@gmail.com>
2024-03-12 15:50:25 +0700

    fix: product list

--- src/pages/SearchProducts/product-list.styled-components.js
+++ src/pages/SearchProducts/product-list.styled-components.js
@@ -3,7 +3,7 @@ import { styled } from "../../stitches.config";
 export const ContentWrapper = styled('div', {
     '& .inner-wrapper': {
         display: 'grid',
-        gridTemplateColumns: 'repeat(5, auto)',
+        gridTemplateColumns: 'repeat(5, 1fr)',
         gap: '25px',
         '& > div': {
             maxWidth: '236px',
